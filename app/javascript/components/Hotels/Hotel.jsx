be93d86b826e74d3b3e8207808fe4e7d13f6cd5f
Sergii <sergykoretsfsp@gmail.com>
2018-10-07 15:03:18 +0300

    modile version

--- app/javascript/components/Hotels/Hotel.jsx
+++ app/javascript/components/Hotels/Hotel.jsx
@@ -56,10 +56,9 @@ export default class Hotel extends React.Component {
         { images.length > 0 &&
           <Fragment>
             <ImageGallery items={images} additionalClass='custom-gallery' />
-            <hr/>
           </Fragment>}
-        <div className='row'>
-          <div className='col-lg-9'>
+        <div className='info-block'>
+          <div className='hotel-info'>
             <div className='hotel-header'>
               <h2>{this.state.hotel.name}</h2>
               <span>{this.state.hotel.price} {this.state.hotel.price && 'UAH'}</span>
@@ -68,18 +67,15 @@ export default class Hotel extends React.Component {
               <span dangerouslySetInnerHTML={{__html: this.state.hotel.description}}></span>
             </div>
           </div>
-          <div className='col-lg-3 left-border text-center'>
-            <div className='rating'>
-              <Rater initialRating={parseFloat(this.state.hotel.googleRating)} emptySymbol="fa fa-star-o fa-2x"
-                     fullSymbol="fa fa-star fa-2x" readonly className='hotel-stars'/>
-              <span className='rating-number'>{this.state.hotel.googleRating}</span>
-            </div>
+          <div className='actions-block'>
+            <Rater initialRating={parseFloat(this.state.hotel.googleRating)} emptySymbol="fa fa-star-o fa-2x"
+                   fullSymbol="fa fa-star fa-2x" readonly className='hotel-stars'/>
             <div className='phones'>
               { this.state.hotel.phones.map((phone, i) => {
                 return (
-                  <div className='phone' key={i}>
+                  <a href={`tel:${phone}`} className='phone' key={i}>
                     {phone}
-                  </div>
+                  </a>
                 )})}
             </div>
             <div className='hotel-buttons text-center'>
@@ -91,46 +87,41 @@ export default class Hotel extends React.Component {
             </div>
           </div>
         </div>
-        <hr/>
-        <div className='row'>
-          <div className='col-lg-9'>
-            <div className='reviews-wrap'>
-              <div className='review-form'>
-                <textarea type='text' rows='3' onChange={(e) => this.handleInputChange('review', e.target.value)} value={this.state.review} className='form-control'/>
-                <div className='review-actions'>
-                  <button className='btn btn-dark' onClick={this.submitReview} disabled={this.state.reviewRating == 0}>Залишити відгук</button>
-                  <Rater start={0} stop={5} step={1} onClick={(rate) => this.handleInputChange('reviewRating', rate)} emptySymbol="fa fa-star-o fa-2x"
-                         fullSymbol="fa fa-star fa-2x" className='hotel-stars' initialRating={this.state.reviewRating}/>
-                </div>
-              </div>
-              <div className='reviews'>
-                { this.state.hotel.googleReviews.map((review, i) => {
-                  return (
-                    <Fragment key={i}>
-                      <hr/>
-                      <div className='review'>
-                        <div className={ review.id ? 'round-image-200' : '' }><img className='review-avatar' src={review.avatar} alt={review.author} /></div>
-                        <div className='review-content'>
-                          <div className='content-top'>
-                            <div className='left'>
-                              <span className='author'>{review.author}</span>
-                              <span className='date'>{review.created}</span>
-                            </div>
-                            <div className='right'>
-                              <Rater initialRating={parseInt(review.rating, 10)} emptySymbol="fa fa-star-o"
-                                     fullSymbol="fa fa-star" readonly className='hotel-stars'/>
-                              { review.destroyable && <i className='fa fa-trash-o' onClick={() => this.deleteReview(review.id)}/>}
-                            </div>
-                          </div>
-                          <div className='review-body'>
-                            {review.text}
-                          </div>
+        <div className='reviews-wrap'>
+          <div className='review-form'>
+            <textarea type='text' rows='3' onChange={(e) => this.handleInputChange('review', e.target.value)} value={this.state.review} className='form-control'/>
+            <div className='review-actions'>
+              <button className='btn btn-dark' onClick={this.submitReview} disabled={this.state.reviewRating == 0}>Залишити відгук</button>
+              <Rater start={0} stop={5} step={1} onClick={(rate) => this.handleInputChange('reviewRating', rate)} emptySymbol="fa fa-star-o fa-2x"
+                     fullSymbol="fa fa-star fa-2x" className='hotel-stars' initialRating={this.state.reviewRating}/>
+            </div>
+          </div>
+          <div className='reviews'>
+            { this.state.hotel.googleReviews.map((review, i) => {
+              return (
+                <Fragment key={i}>
+                  <hr/>
+                  <div className='review'>
+                    <div className={ review.id ? 'round-image-200' : 'review-avatar-div' }><img className='review-avatar' src={review.avatar} alt={review.author} /></div>
+                    <div className='review-content'>
+                      <div className='content-top'>
+                        <div className='left'>
+                          <span className='author'>{review.author}</span>
+                          <span className='date'>{review.created}</span>
+                        </div>
+                        <div className='right'>
+                          <Rater initialRating={parseInt(review.rating, 10)} emptySymbol="fa fa-star-o"
+                                 fullSymbol="fa fa-star" readonly className='hotel-stars'/>
+                          { review.destroyable && <i className='fa fa-trash-o' onClick={() => this.deleteReview(review.id)}/>}
                         </div>
                       </div>
-                    </Fragment>
-                  )})}
-              </div>
-            </div>
+                      <div className='review-body'>
+                        {review.text}
+                      </div>
+                    </div>
+                  </div>
+                </Fragment>
+              )})}
           </div>
         </div>
       </div>
