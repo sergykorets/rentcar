9175aca80af943ce9231449f7d998ded5e0c2d7d
Sergii <sergykoretsfsp@gmail.com>
2018-10-08 23:03:21 +0300

    diff domain login fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,13 @@
 class SessionsController < Devise::SessionsController
+  before_action :set_session, only: :new
+
   def new
-    session[:review_url] = "/hotels/#{params[:hotel]}?comment=#{params[:comment]}"
     super
+  end
+
+  private
+
+  def set_session
+    session[:review_url] = "/hotels/#{params[:hotel]}?comment=#{params[:comment]}"
   end
 end
