3bd30f1c70faf74c192ac49fdb236f279915468e
Sergii <sergykoretsfsp@gmail.com>
2018-10-16 21:27:12 +0300

    skipass

--- app/controllers/schemas_controller.rb
+++ app/controllers/schemas_controller.rb
@@ -2,4 +2,6 @@ class SchemasController < ApplicationController
   def index
     @admin = Rails.env.development? || (current_user && current_user.admin)
   end
+
+  def skipass;  end
 end
--- app/javascript/components/Hotels/Hotels.jsx
+++ app/javascript/components/Hotels/Hotels.jsx
@@ -45,8 +45,7 @@ export default class Hotels extends React.Component {
         <div className='introduction'>
           <p>Основою цього сайту є <strong><b>Google Maps API</b></strong>. Готелі, кафе, фото та відгуки до них автоматично оновлюються разом з тим, що є на Google картах. Місцезнаходження закладів
             можна подивитися на <strong><b>3D карті</b></strong>. Готелі, які присутні на <strong><b>Booking.com</b></strong> також показуються на цьому сайті. Якщо Ваш заклад відсутній на сайті,
-            то Ви можете його створити в меню "Додати заклад" (потрібна реєстрація на сайті, яка займає 1 хвилину), або зв'язатися зі мною
-            (email для зв'язку знаходиться знизу). Також вітаються ідеї щодо покращення сайту.</p>
+            то Ви можете його створити в меню "Додати заклад" (потрібна реєстрація на сайті, яка займає 1 хвилину), або зв'язатися зі мною: sergykoretsfsp@gmail.com</p>
           <div className='facebook'>
             <iframe src="https://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.dragobrat.net&width=51&layout=box_count&action=like&size=small&show_faces=true&share=true&height=65&appId=783416265322787" width="51" height="65" style={{border:'none',overflow:'hidden'}} scrolling="no" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
           </div>
