42963ac6616f3815e2c6c276f929a9f40f011e4e
Sergii <sergykoretsfsp@gmail.com>
2018-12-29 14:46:42 +0200

    floor fix

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,7 +2,7 @@ class Room < ApplicationRecord
   belongs_to :hotel
   has_many :reservations
 
-  validates :number, :floor, :places, presence: true, numericality: { greater_than: 0 }
+  validates :number, :floor, :places, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates_uniqueness_of :number, scope: :hotel_id
 
   accepts_nested_attributes_for :reservations
