65741184033a76e2324a0acf972f8a4dc026cf0b
Sergii Fedorchuk <ext-sergii.fedorchuck@foodie.fm>
2019-09-12 14:36:34 +0300

    receive fix

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,14 +3,15 @@ class ApiController < ActionController::Base
   def receive
     if params[:email] == 'vasilpipash@gmail.com'
       hotel = Hotel.where("name ILIKE ?", "%#{params[:hotel]}%").first
-      reservations = JSON.parse(params[:reservations].to_json)
-      reservations.each do |reservation|
-        r = reservation.last
-        room = hotel.rooms.where(number: r['roomId']).first
-        hotel.reservations.create(room_id: room.id,
-                                   name: r['name'], phone: r['phone'], places: r['places'],
-                                   description: r['description'], deposit: r['deposit'],
-                                   start_date: r['startDate'], end_date: r['endDate'], status: r['status'])
+      reservations = JSON.parse(params[:reservations])
+      reservations.each do |r|
+        room_id = hotel.rooms.find_by_number(r['roomId']).try(:id)
+        reservation = {room_id: room_id,
+                       name: r['name'], phone: r['phone'], places: r['places'],
+                       description: r['description'], deposit: r['deposit'],
+                       start_date: r['startDate'], end_date: r['endDate'], status: r['status']}
+        next if hotel.reservations.exists?(reservation)
+        hotel.reservations.create(reservation)
       end
       render json: {message: "Update for hotel #{params[:hotel]} done"}
     else
